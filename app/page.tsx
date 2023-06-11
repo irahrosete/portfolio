f7f63c18de9b75e5be4cbf81e2aca36dcd330c8b
irah rosete <72952642+irahrosete@users.noreply.github.com>
2023-06-12 00:52:20 +1000

    update styles

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,16 @@ const Home = async () => {
   const projects = await getProjects()
   return (
     <div className='max-w-5xl mx-auto'>
-      <h1 className='text-9xl font-extrabold pt-20'>Hello! I am 
+      <h1 className='text-9xl font-extrabold text-center pt-20'>Hello! I am 
         <span className='bg-gradient-to-r 
-          from-blue-500 
-          to-red-400 
+          from-blue-300 
+          to-blue-300 
           bg-clip-text 
           text-transparent'> Irah
         </span>.
       </h1>
-      <p className='mt-2 text-3xl'>I&apos;m a designer and a developer based in Sydney.</p>
-      <h2 className='mt-24 font-bold text-4xl'>My projects</h2>
+      <p className='mt-2 text-3xl text-center'>I&apos;m a designer and a developer based in Sydney.</p>
+      <h2 className='mt-24 mb-8 text-center text-4xl'>Check out some of my latest works</h2>
       <div className='mt-5 grid grid-cols-1 md: grid-cols-2 lg:grid-cols-3 gap-8'>
         {projects.map((project) => (
           <Link href={`/projects/${project.slug}`} key={project._id} 
@@ -29,12 +29,8 @@ const Home = async () => {
                 className='object-cover rounded-lg border border-gray-500'
               />
             )}
-            <div className='mt-2 font-extrabold bg-gradient-to-r 
-              from-blue-500 
-              to-red-400 
-              bg-clip-text 
-              text-transparent'>{project.title}
-            </div>
+            <div className='mt-2 text-2xl font-bold uppercase'>{project.title}</div>
+            <div className='uppercase'>{project.category}</div>
           </Link>
         ))}
       </div>
