d8367e3cffaa27c9ebe4eaf1afb6a0a18a813023
irah rosete <72952642+irahrosete@users.noreply.github.com>
2023-06-12 17:09:05 +1000

    add page schema, typoe and utils

--- sanity/utils.ts
+++ sanity/utils.ts
@@ -1,6 +1,7 @@
 import { Project } from '@/types/Project'
 import { createClient, groq } from 'next-sanity'
 import clientConfig from './config/client-config'
+import { Page } from '@/types/Page'
 
 export const getProjects = async (): Promise<Project[]> => {
   const client = createClient(clientConfig)
@@ -47,4 +48,32 @@ export const getProject = async (slug: string): Promise<Project> => {
     }`,
     { slug }
   )
-} 
+}
+
+export const getPages = async (): Promise<Page[]> => {
+  const client = createClient(clientConfig)
+
+  return client.fetch(
+    groq`*[_type == 'page']{
+      _id,
+      _createdAt,
+      title,
+      'slug': slug.current
+    }`
+  )
+}
+
+export const getPage = async (slug: string): Promise<Page> => {
+  const client = createClient(clientConfig)
+
+  return client.fetch(
+    groq`*[_type == 'page' && slug.current == $slug][0]{
+      _id,
+      _createdAt,
+      title,
+      'slug': slug.current,
+      content
+    }`,
+    { slug }
+  )
+}
